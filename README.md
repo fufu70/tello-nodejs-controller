c62695ede7701398c6dafe519e276cae19631e7d
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-09-20 23:43:53 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tello-NodeJS-Controller
+
+Controls a tello quadrotor through a UDP connection.
+
+### Note
+
+Make sure to connect to the tello bot before starting up the server.
---
+++ controller.js
@@ -0,0 +1,32 @@
+var dgram = require('dgram');
+var stdio = require('stdio');
+var PORT_SEND = 8889;
+var HOST = '192.168.10.1';
+
+var client = dgram.createSocket('udp4');
+
+client.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+    getCommand();
+});
+
+client.bind(PORT_SEND);
+
+function sendCommand(message) {
+
+    if (message == 'quit') {
+        client.close(); return;
+    }
+
+    var message = new Buffer.from(message);
+    client.send(message, 0, message.length, PORT_SEND, HOST, function(err, bytes) {
+    });
+}
+
+function getCommand() {
+    stdio.question('> ', function (err, command) {
+        sendCommand(command);
+    });
+}
+
+getCommand();
